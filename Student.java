956d7216a9c5c45e166c17289dc5154fc1f46ceb
SnekaSekar05 <sneka@ambigai.com>
2025-06-05 10:59:15 +0530

    example of Class and object program using java

---
+++ Student.java
@@ -0,0 +1,31 @@
+class Main{
+    public static void main(String[] args) {
+    Student std=new Student("sneka",101);
+    std.displayDetails();
+    std.ChangeSchilName("Holly Cross");
+}
+
+}
+
+public class Student {
+
+    public String name;
+    public int rollNo;
+    public static String schlName = "ABC Public School";   
+
+    public Student(String name, int rollNo) {
+        this.name = name;
+        this.rollNo = rollNo;
+    }
+
+    public void displayDetails() {
+        System.out.println("Student Name: " + name);
+        System.out.println("Student Roll No: " + rollNo);
+        System.out.println("Student School Name: " + schlName);
+    }
+
+    public void ChangeSchilName(String school) {
+        schlName = school;
+        System.out.println("The new school name: " + schlName);
+    }
+}
